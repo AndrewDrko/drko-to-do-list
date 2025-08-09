226df1daa619a883188f9d82994ce685cff97f4a
AndrewDrko <andrewmccartney09@gmail.com>
2025-08-08 18:36:35 -0600

    Refactor

--- src/components/App.js
+++ src/components/App.js
@@ -6,37 +6,6 @@ import { Aside } from "./Aside";
 import { Main } from "./Main";
 import { Modal } from "./Modal";
 
-const initialData = [
-  {
-    id: 1,
-    task: "Cocinarle una torta de huevito a mi Alita",
-    date: "2025-08-07",
-    priority: "high",
-    completed: false,
-  },
-  {
-    id: 2,
-    task: "Bailar ca'huates pistaches con Dany!!",
-    date: "2025-08-07",
-    priority: "medium",
-    completed: true,
-  },
-  {
-    id: 3,
-    task: "Declararle matrimonio a mi Alita hemoshaaaa 💞",
-    date: "2025-08-07",
-    priority: "high",
-    completed: true,
-  },
-  {
-    id: 4,
-    task: "Comer pastel de Pau en la noche para vivir para morir para manifestarse por las animas del pulgatorio que todo salga como tenga que salir mi pana echele mi compa y amo a mi novia alee jejeje",
-    date: "2025-08-08",
-    priority: "low",
-    completed: false,
-  },
-];
-
 function App() {
   const initialValues = JSON.parse(localStorage.getItem("tasks")) || [];
 
